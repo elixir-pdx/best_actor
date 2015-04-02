1fb1d893a6b0da95469eb335abce6074b86144ac
Colin Lebens <colin.lebens@gmail.com>
2015-04-01 20:48:32 -0700

    Solution

--- lib/actor.ex
+++ lib/actor.ex
@@ -1,3 +1,9 @@
 defmodule Actor do
-
+  def vote(x) do
+    receive do
+      {:ballot, tally, num_actors} -> 
+        :random.seed(:erlang.now)
+        send tally, {:filled_ballot, :random.uniform(num_actors)-1}
+    end
+  end
 end
