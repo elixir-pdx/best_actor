41363a5208ddb7a9764ab2cf300dca4fb1ee79b9
Kenneth Brown <pliny@ptelder.net>
2015-04-01 20:50:25 -0700

    It sort of works...

--- lib/actor.ex
+++ lib/actor.ex
@@ -1,3 +1,8 @@
 defmodule Actor do
-
+	def vote(host, actorList) do
+		:random.seed(:erlang.now)
+		choice = Enum.at(actorList, :random.uniform(Enum.count(actorList)) - 1)
+		#choice = "Rigged vote"
+		send host, {:ballot, choice}
+	end
 end
--- lib/host.ex
+++ lib/host.ex
@@ -1,11 +1,30 @@
 defmodule Host do
-  def greet do 
+  def greet(totalVoters, tally) do 
     receive do
       {:choke, msg} -> 
         IO.puts("Derp! Derp! #{msg} I'm dying up here!")
-      {:shine, msg} -> 
-        IO.puts("Good evening folks and welcome to the #{msg}")
-        greet()
+			{:ballot, msg} ->
+				IO.puts("Vote received for #{msg}!")
+				tally = tally ++ [msg]
+				count = Enum.count(tally)
+				if count == totalVoters do
+					result = Enum.group_by(tally, fn(x) -> x end)
+					countedResult = Enum.map(result, fn {k,v} -> {k, Enum.count(v)} end)
+					rankedResult = Enum.sort(countedResult, fn ({actor, votes},{actor2, votes2}) -> (votes > votes2) end)
+					[winner|runnerUps] = rankedResult
+					{winnerName, winnerVotes} = winner
+					if Enum.count(runnerUps) > 0 do
+						{runnerUpName, runnerUpVotes} = List.first(runnerUps)
+						IO.puts("The runner up is #{runnerUpName} with #{runnerUpVotes} votes")
+					end
+					IO.puts("The winner is #{winnerName}! With #{winnerVotes} votes!")
+				else
+					greet(totalVoters, tally)
+				end
     end
   end
 end
+
+# "Niel"
+#
+#   { "Neil": ["Neil", "Neil"]
