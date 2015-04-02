b2b075e55c8c2b98c291f67196bbf9d293bdb754
Your Name <me@mchlwrrn.com>
2015-04-02 03:55:46 +0000

    solution

--- lib/show.ex
+++ lib/show.ex
@@ -1,6 +1,8 @@
 defmodule Show do
   def begin do
-    jimmy = spawn(Host.greet)
+    jimmy = spawn(Host, :greet, [])
+    actors = [:bob, :sally, :joe, :larry, :emily]
+    send jimmy, {:tally, [actors]}
     send jimmy, {:choke, "2nd Elixir Games PDX Awards!"}
   end
 end
