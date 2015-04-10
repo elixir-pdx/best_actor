b599f802c468704c5cd6f8c1c50e76c1dc1e4f00
binaryseed <vincefoley@gmail.com>
2015-04-09 20:57:14 -0700

    Solution with Actor, Task

--- lib/actor.ex
+++ lib/actor.ex
@@ -1,3 +1,15 @@
 defmodule Actor do
+  def vote(_actor, nominees) do
+    pick = random(Enum.count(nominees) - 1)
 
+    nominees
+      |> Enum.at(pick)
+      |> Host.vote
+  end
+
+  def random(n) do
+    << a :: 32, b :: 32, c :: 32 >> = :crypto.rand_bytes(12)
+    :random.seed(a, b, c)
+    :random.uniform(n)
+  end
 end
